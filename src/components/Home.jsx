a3f431292a3e0aae804e2239ba71f96b0bc36714
Martín Pravia <marto.pravia@gmail.com>
2025-07-17 11:26:37 -0300

    axios para tweet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [tweets, setTweets] = useState([]);
+  useEffect(() => {
+    const getTweets = async () => {
+      const response = await axios({
+        method: "GET",
+        url: "http://localhost:3000/tweets",
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2ODY0Mzg0NDE4N2JiMzc5ZDk5Yjg0YzIiLCJpYXQiOjE3NTI3NjE2MzN9.7S6zkIDnsbsMDnmdJB-VRcZhc74hlouLvgU-wEIeZII`,
+        },
+      });
+      console.log(response.data);
+      setTweets(response.data);
+    };
+    getTweets();
+  }, []);
   return <div>Home</div>;
 };
 
