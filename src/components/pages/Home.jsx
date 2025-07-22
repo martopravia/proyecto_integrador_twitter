0a919df54ed9345bb966850fb6fcd0f0ebbb33c7
Martín Pravia <marto.pravia@gmail.com>
2025-07-22 14:40:46 -0300

    profile view

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
   const dispatch = useDispatch();
   const [newTweetText, setNewTweetText] = useState("");
   const [submittedTweets, setSubmittedTweets] = useState(0);
-  const username = useSelector((state) => state.user.username);
 
+  const username = useSelector((state) => state.user.username);
   useEffect(() => {
     const getTweets = async () => {
       const response = await axios({
@@ -49,44 +49,84 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <h3>Home</h3>
-      <Link to={`/${username}`}>Profile</Link>
-      <img
-        src={isLocalImage ? `http://localhost:3000/img/${userImg}` : userImg}
-        alt="profile image"
-      />
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-6 min-vh-100 border">
+            <div className="py-3 ">
+              <h5 className="fw-bold">Home</h5>
+            </div>
+            <div className="d-flex  py-3 border-bottom">
+              <img
+                src={
+                  isLocalImage
+                    ? `http://localhost:3000/img/${userImg}`
+                    : userImg
+                }
+                className="rounded-circle me-3"
+                width="50"
+                height="50"
+                alt="Profile"
+              />
+              <form className="flex-grow-1" id="tweetForm">
+                <textarea
+                  id="newTweet"
+                  class="form-control mb-2"
+                  rows="2"
+                  placeholder="What's happening?"
+                ></textarea>
+                <div className="text-end">
+                  <button
+                    type="submit"
+                    className="btn btn-primary rounded-pill"
+                  >
+                    Tweet
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3>Home</h3>
+        <Link to={`/${username}`}>Profile</Link>
+        <img
+          src={isLocalImage ? `http://localhost:3000/img/${userImg}` : userImg}
+          alt="profile image"
+        />
 
-      <form action="submit" onSubmit={handleSubmit}>
-        <label htmlFor="newTweet"></label>
-        <textarea
-          name="newTweet"
-          id="newTweet"
-          style={{ height: 50 }}
-          placeholder="What's happening?"
-          value={newTweetText}
-          onChange={(e) => setNewTweetText(e.target.value)}
-        ></textarea>
-        <button className="btn btn-primary rounded-pill">Tweet</button>
-      </form>
+        <form action="submit" onSubmit={handleSubmit}>
+          <label htmlFor="newTweet"></label>
+          <textarea
+            name="newTweet"
+            id="newTweet"
+            style={{ height: 50 }}
+            placeholder="What's happening?"
+            value={newTweetText}
+            onChange={(e) => setNewTweetText(e.target.value)}
+          ></textarea>
+          <button className="btn btn-primary rounded-pill">Tweet</button>
+        </form>
 
-      <ul className="p-0" style={{ listStyleType: "none" }}>
-        {latestTweets.map((tweet) => (
-          <li key={tweet._id}>
-            <OneTweet
-              text={tweet.text}
-              firstname={tweet.user.firstname}
-              lastname={tweet.user.lastname}
-              username={tweet.user.username}
-              profileImg={tweet.user.profileImg}
-              createdAt={tweet.createdAt}
-              likes={tweet.likes}
-              tweetId={tweet._id}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
+        <ul className="p-0" style={{ listStyleType: "none" }}>
+          {latestTweets.map((tweet) => (
+            <li key={tweet._id}>
+              <OneTweet
+                text={tweet.text}
+                firstname={tweet.user.firstname}
+                lastname={tweet.user.lastname}
+                username={tweet.user.username}
+                profileImg={tweet.user.profileImg}
+                createdAt={tweet.createdAt}
+                likes={tweet.likes}
+                tweetId={tweet._id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
