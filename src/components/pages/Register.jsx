402667271535b1d4b578a83f3819af0f384f22f5
BBocalandro <bocalandrobruno@gmail.com>
2025-07-23 09:15:25 -0300

    Maquetado para las pages de Login y Register

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router";
+import "./Register.css";
 
 const Register = () => {
   const [firstname, setFirstName] = useState("");
@@ -37,82 +38,125 @@ const Register = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleRegister}>
-        <label htmlFor="firstname" hidden>
-          firstname
-        </label>
-        <input
-          type="text"
-          name="firstname"
-          id="firstname"
-          placeholder="First name"
-          value={firstname}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <label htmlFor="lastname" hidden>
-          lastname
-        </label>
-        <input
-          type="text"
-          name="lastname"
-          id="lastname"
-          placeholder="Last name"
-          value={lastname}
-          onChange={(e) => setLastName(e.target.value)}
-        />{" "}
-        <label htmlFor="email" hidden>
-          email
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />{" "}
-        <label htmlFor="username" hidden>
-          username
-        </label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUserName(e.target.value)}
-        />{" "}
-        <div className="mb-3">
-          <label htmlFor="profilePicture" className="form-label" hidden>
-            Profile Picture
-          </label>
-          <input
-            className="form-control"
-            type="file"
-            id="profilePicture"
-            name="profilePicture"
-            onChange={(e) => setProfileImg(e.target.files[0])}
-          />
+    <div className="container d-flex align-items-center justify-content-center">
+      <div className="row w-100">
+        <div className="d-sm-none d-md-flex col-md-4 col-lg-7 welcomeRegisterBanner rounded-start d-flex flex-column justify-content-between p-5">
+          <i className="bi bi-twitter-x fs-1" style={{ color: "white" }}></i>
+          <p className="fs-3 fw-semibold m-0">Hi! Welcome to X clone.</p>
         </div>
-        <label htmlFor="password" hidden>
-          Password
-        </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Sign up!</button>
-      </form>
+        <div className="col-sm-12 col-md-8 col-lg-5 loginForm rounded-end px-5 d-flex flex-column justify-content-center">
+          <form onSubmit={handleRegister} className="mb-5">
+            <legend className="fs-1 fw-semibold mb-0">Sign up</legend>
+            <p className="fs-5 fw-semibold mb-5">
+              Create an account and start using X
+            </p>
+            <div className="mb-3">
+              <label htmlFor="firstname" className="form-label" hidden>
+                First Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="firstname"
+                id="firstname"
+                value={firstname}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder="First name"
+              />
+            </div>
 
-      <p>
-        <Link to={"/login"}>Sign in</Link>
-      </p>
-    </>
+            <div className="mb-3">
+              <label htmlFor="lastname" className="form-label" hidden>
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="lastname"
+                id="lastname"
+                value={lastname}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="Last name"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label" hidden>
+                email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label" hidden>
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                id="username"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+                placeholder="Username"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="profilePicture" className="form-label" hidden>
+                Profile Picture
+              </label>
+              <input
+                className="form-control"
+                type="file"
+                id="profilePicture"
+                name="profilePicture"
+                onChange={(e) => setProfileImg(e.target.files[0])}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label" hidden>
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="d-grid">
+              <button
+                className="btn btn-primary text-white rounded-pill"
+                type="submit"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+
+          <p className="text-center ">
+            Already have an account?{" "}
+            <Link className="text-decoration-none" to={"/login"}>
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
